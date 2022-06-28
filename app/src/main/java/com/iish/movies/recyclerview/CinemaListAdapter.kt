03679d02993a377e05f71c947311f2a87e436904
iishutin <iishutin@zuzex.com>
2022-06-28 13:06:00 +0300

    get data from api

---
+++ app/src/main/java/com/iish/movies/recyclerview/CinemaListAdapter.kt
@@ -0,0 +1,40 @@
+package com.iish.movies.recyclerview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.iish.movies.databinding.CustomRecyclerviewLayoutBinding
+import com.iish.movies.model.Cinema
+import com.iish.movies.recyclerview.viewholder.CinemaViewHolder
+
+class CinemaListAdapter : ListAdapter<Cinema, CinemaViewHolder>(DiffCallback()) {
+
+    private var data: List<Cinema>? = null
+
+    fun updData(data: List<Cinema>){
+        this.data = data
+    }
+
+    override fun onBindViewHolder(holder: CinemaViewHolder, position: Int) {
+        holder.bind(data!![position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CinemaViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = CustomRecyclerviewLayoutBinding.inflate(inflater, parent, false)
+        return CinemaViewHolder(binding)
+    }
+
+    private class DiffCallback : DiffUtil.ItemCallback<Cinema>() {
+
+        override fun areItemsTheSame(oldItem: Cinema, newItem: Cinema): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Cinema, newItem: Cinema): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+}
